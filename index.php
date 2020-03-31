361eb4a74e02f4974f9e37c5f1f76a90154d5ef4
David <pro.davidsaoud@gmail.com>
2020-03-31 15:40:53 +0200

    fix: get route commented

--- index.php
+++ index.php
@@ -7,12 +7,13 @@ header('Content-Type: application/json;charset=utf-8');
 $req_method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['PATH_INFO'];
 
-if($path === '/api/v1/hero' && $req_method === 'GET') {
-  $hero = new Personage('Jean-Michel', 100);
-  $response = array('info' => $hero->infos());
-  echo json_encode($response);
+// if($path === '/api/v1/hero' && $req_method === 'GET') {
+//   $hero = new Personage('Jean-Michel', 100);
+//   $response = array('info' => $hero->infos());
+//   echo json_encode($response);
 
-} else if($path === '/api/v1/create/hero' && $req_method === 'POST') {
+// } else 
+if($path === '/api/v1/create/hero' && $req_method === 'POST') {
   $data = json_decode(file_get_contents('php://input'), true);
   $hero = new Personage($data['name'], $data['hp']);
   $response = array('info' => $hero->infos());
