8c9957188a2c35049147efac9355f0bfd1d20a65
David <pro.davidsaoud@gmail.com>
2020-04-01 09:11:07 +0200

    fix: map + refacto

--- src/js/game.js
+++ src/js/game.js
@@ -10,6 +10,8 @@ submit.addEventListener('click', e => {
   const form = document.getElementById('form');
   // Img
   const img = document.getElementById('img');
+  // Map
+  const map = document.getElementById('map');
   // Values from input
   const name = document.getElementById('name').value;
   const hp = parseInt(document.getElementById('hp').value);
@@ -25,30 +27,31 @@ submit.addEventListener('click', e => {
     .then(res => (post.innerText += res.info))
     .then((form.style.display = 'none'))
     .then(() => {
-      img.style.display = 'block';
+      img.style.display = 'block',
+      map.style.display = 'block';
     })
     .then(() => {
       // Global state
       let x = 0,
         y = 0,
-        speed = 10;
-      let keyState = {};
+        speed = 5,
+        keyState = {};
 
       document.addEventListener('keyup', e => {
         keyState[e.keyCode] = false;
 
         switch (e.keyCode) {
           case 81:
-            img.src = 'src/img/gauche1.png';
+            img.src = 'src/img/characters/gauche1.png';
             break;
           case 90:
-            img.src = 'src/img/haut1.png';
+            img.src = 'src/img/characters/haut1.png';
             break;
           case 68:
-            img.src = 'src/img/droite1.png';
+            img.src = 'src/img/characters/droite1.png';
             break;
           case 83:
-            img.src = 'src/img/bas1.png';
+            img.src = 'src/img/characters/bas1.png';
             break;
         }
       });
@@ -60,19 +63,19 @@ submit.addEventListener('click', e => {
       function gameLoop() {
         if (keyState[81]) {
           perso.style.left = `${speed * x--}px`;
-          img.src = 'src/img/moveLeft.gif';
+          img.src = 'src/img/characters/moveLeft.gif';
         }
         if (keyState[90]) {
           perso.style.top = `${speed * y--}px`;
-          img.src = 'src/img/moveUp.gif';
+          img.src = 'src/img/characters/moveUp.gif';
         }
         if (keyState[68]) {
           perso.style.left = `${speed * x++}px`;
-          img.src = 'src/img/moveRight.gif';
+          img.src = 'src/img/characters/moveRight.gif';
         }
         if (keyState[83]) {
           perso.style.top = `${speed * y++}px`;
-          img.src = 'src/img/moveDown.gif';
+          img.src = 'src/img/characters/moveDown.gif';
         }
         setTimeout(gameLoop, 50);
       }
